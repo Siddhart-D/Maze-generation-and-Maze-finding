71ff57c86a698a84fa257736e26ba2788824371b
Siddharth <93031748+Siddhart-D@users.noreply.github.com>
2022-06-06 17:45:32 +0530

    addinglittleupdate

--- Maze_generation.py
+++ Maze_generation.py
@@ -2,13 +2,12 @@ import random
 import numpy as np
 import turtle
 
-graphsize,cellthickness,wallthickness = 30,15,5
+graphsize, cellthickness, wallthickness = 30,15,5
 
 def sumoflists(input):
     sum_l = 0
     for row in input:
         sum_l += sum(row)
-
 
 def mazegeneration(x_axis,y_axis):
     walls = [[[1,1,1,1,1] for a in range(x_axis)] for b in range(y_axis)]
